6c654a241053acacd87345a7a02180018bbf5c6b
Sanjeev Tamang <sktamang@deerwalk.com>
2015-10-16 09:17:51 +0545

    aggregator completed

---
+++ CascadingLearning/src/main/java/implementations/RaiseCalculation.java
@@ -0,0 +1,25 @@
+package implementations;
+
+import cascading.flow.FlowProcess;
+import cascading.operation.BaseOperation;
+import cascading.operation.Function;
+import cascading.operation.FunctionCall;
+import cascading.tuple.Fields;
+import cascading.tuple.TupleEntry;
+
+/**
+ * Created by sktamang on 10/15/15.
+ */
+public class RaiseCalculation extends BaseOperation implements Function {
+    static Fields fieldOutput = new Fields(
+            "name", "raiseAge");
+    public RaiseCalculation(){
+        super(6,fieldOutput);
+    }
+
+    @Override
+    public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
+        TupleEntry entry=functionCall.getArguments();
+        System.out.println(entry);
+    }
+}
